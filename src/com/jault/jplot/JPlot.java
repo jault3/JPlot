cea589eb81ad36e8e6674caee3987e06b49e6124
Josh Ault <josh.ault3@gmail.com>
2012-12-13 10:38:10 -0600

    adding back all java files

---
+++ src/com/jault/jplot/JPlot.java
@@ -0,0 +1,45 @@
+package com.jault.jplot;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+import javax.swing.JFrame;
+
+import com.jault.jplot.helpers.Constants;
+
+public class JPlot extends JFrame {
+	
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = -8344597455042452839L;
+	
+	Grid grid;
+	
+	public JPlot() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        getContentPane().setPreferredSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        
+        grid = Grid.getInstance();
+        
+		getContentPane().add(grid);
+		
+		pack();
+		setVisible(true);
+		
+		/*grid.addPoint(new Point(10,20));
+		grid.addPoint(new Point(10,30));
+		grid.addPoint(new Point(10,40));
+		grid.addPoint(new Point(20,20));
+		grid.addPoint(new Point(40,30));
+		grid.addPoint(new Point(60,5));*/
+		grid.addPoint(new Point(0,0));
+		grid.addPoint(new Point(12,12));
+		grid.addPoint(new Point(24,24));
+		grid.addPoint(new Point(36,36));
+		grid.addPoint(new Point(48,48));
+		grid.addPoint(new Point(60,60));
+	}
+
+}
---
+++ src/com/jault/jplot/tests/PlotPanelTest.java
@@ -0,0 +1,40 @@
+package com.jault.jplot.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jault.jplot.PlotPanel;
+
+public class PlotPanelTest {
+
+	@Test
+	public void testRound() {
+		PlotPanel p = new PlotPanel();
+		Assert.assertEquals(0, p.round(0));
+		Assert.assertEquals(0, p.round(1));
+		Assert.assertEquals(0, p.round(2));
+		Assert.assertEquals(5, p.round(3));
+		Assert.assertEquals(5, p.round(4));
+		Assert.assertEquals(5, p.round(5));
+		Assert.assertEquals(5, p.round(6));
+		Assert.assertEquals(5, p.round(7));
+		Assert.assertEquals(10, p.round(8));
+		Assert.assertEquals(10, p.round(9));
+		Assert.assertEquals(10, p.round(10));
+		Assert.assertEquals(10, p.round(11));
+		Assert.assertEquals(10, p.round(12));
+		Assert.assertEquals(15, p.round(13));
+		Assert.assertEquals(15, p.round(14));
+		Assert.assertEquals(15, p.round(15));
+		Assert.assertEquals(15, p.round(16));
+		Assert.assertEquals(15, p.round(17));
+		Assert.assertEquals(20, p.round(18));
+		Assert.assertEquals(20, p.round(19));
+		Assert.assertEquals(20, p.round(20));
+		Assert.assertEquals(20, p.round(21));
+		Assert.assertEquals(20, p.round(22));
+		Assert.assertEquals(25, p.round(23));
+		Assert.assertEquals(25, p.round(24));
+	}
+
+}
